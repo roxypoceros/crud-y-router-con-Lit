3af718d3d3d5ae4b9e90247ef787ea5c07d896ae
roxypoceros <roxypoceros@gmail.com>
2022-12-16 09:01:47 -0600

    Ya funciona el ruteado con los botones

--- components/info-users.js
+++ components/info-users.js
@@ -1,18 +1,16 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
+import { navigator } from "lit-element-router";
 
-export class InfoUsers extends LitElement {
-  static styles = [
-    css`
-      :host {
-        display: block;
-      }
-    `
-  ];
-
+export class InfoUsers extends navigator(LitElement) {
   render() {
     return html`
-    <h1>Estoy en info users</h1>
+      <h1>Estoy en info users</h1>
+      <button @click="${() => this.navigateTo("/")}">Hacia show USERS</button>
     `;
   }
+
+  navigateTo(path) {
+    this.navigate(path);
+  }
 }
-customElements.define('info-users', InfoUsers);
+customElements.define("info-users", InfoUsers);
--- components/show-users.js
+++ components/show-users.js
@@ -1,18 +1,17 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html } from "lit";
+import { navigator } from "lit-element-router";
 
-export class ShowUsers extends LitElement {
-  static styles = [
-    css`
-      :host {
-        display: block;
-      }
-    `
-  ];
-
+export class ShowUsers extends navigator(LitElement) {
   render() {
     return html`
-        <h1>Estoy en show users</h1>
+      <h1>Estoy en show users</h1>
+      <button @click="${() => this.navigateTo("/info")}">
+        Hacia show info
+      </button>
     `;
   }
+  navigateTo(path) {
+    this.navigate(path);
+  }
 }
-customElements.define('show-users', ShowUsers);
+customElements.define("show-users", ShowUsers);
